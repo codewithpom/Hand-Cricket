c022d11aa476816f3ec72da7877d81f373fff58f
Padmashree Jha <codewithpom@gmail.com>
2024-04-13 12:07:22 +0000

    Initial commit

---
+++ html/script.js
@@ -0,0 +1,43 @@
+var socket;
+
+socket = io();
+
+
+socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  })
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 9){
+      messages.push(message);
+      dingSound.currentTime = 0;
+      dingSound.play();
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("join", chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 1000);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+
+function delayReset(){
+  delay = true;
+}
